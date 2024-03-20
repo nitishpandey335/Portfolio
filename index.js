fe32d9166d314cabbc799cb8dcb8aa4f98e40db8
Nitish Kumar Pandey <117139675+nitishpandey335@users.noreply.github.com>
2024-03-20 23:21:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const linkedinIcon = document.querySelector(".fa-linkedin");
+const github = document.querySelector(".fa-github");
+const facebook = document.querySelector(".fa-facebook");
+const instagram = document.querySelector(".fa-instagram");
+
+linkedinIcon.addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/nitishpandey335");
+});
+
+github.addEventListener('click', function() {
+    window.open("https://github.com/nitishpandey335");
+});
+
+facebook.addEventListener('click', function() {
+    window.open("https://www.facebook.com/nitishpandey335");
+});
+
+instagram.addEventListener('click', function() {
+    window.open("https://www.instagram.com/nitishpandey335");
+});
